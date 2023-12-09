a235c4e23cde1cbf50688fa60d80ef44cba31daf
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-12-09 18:17:53 -0300

    task 04 - viewsets profile

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,36 @@
 from rest_framework import viewsets
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if request.method == 'GET':
+            # Crie um contexto com os dados do perfil para passar ao template
+            context = {
+                'profile': instance,
+            }
+
+            # Renderize o template HTML com os detalhes do perfil
+            return render(
+                request,
+                'profile_detail.html',
+                context
+            )
+
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
